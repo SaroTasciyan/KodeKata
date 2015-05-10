fcb8f1e42ba3dfac9d4d04f3d46555ae0003db26
Saro Taşciyan <stasciyan@gmail.com>
2015-05-10 16:11:33 +0300

    Fibonacci Kode Kata

---
+++ KodeKata/Test/Fibonacci.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace KodeKata.Test
+{
+    [TestFixture]
+    public class Fibonacci
+    {
+        public static int[][] FibonacciTestData = new int[][]
+        {
+            new int[] { 1, 1 },
+            new int[] { 2, 1 },
+            new int[] { 3, 2 },
+            new int[] { 4, 3 },
+            new int[] { 5, 5 },
+            new int[] { 6, 8 }
+        };
+
+        [Test]
+        public void NegativeNumberFibonacci()
+        {
+            Assert.Throws<ArgumentException>(() => Utility.Fibonacci(-1));
+        }
+
+        [Test]
+        public void ZeroFibonnaci()
+        {
+            int result = Utility.Fibonacci(0);
+
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        [TestCaseSource("FibonacciTestData")]
+        public void PositiveNumberFibonacci(int number, int expected)
+        {
+            int result = Utility.Fibonacci(number);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
--- KodeKata/Util/Util.cs
+++ KodeKata/Util/Util.cs
@@ -11,5 +11,15 @@ namespace KodeKata
             if (number <= 1) { return 1; } // # Base case: 0! or 1! is 1
             return number * Factorial(number - 1); // # Recursion step: n! = n * (n-1)!
         }
+
+        public static int Fibonacci(int number)
+        {
+            if (number < 0) { throw new ArgumentException("Number can not be negative", "number"); }
+
+            if (number == 0) { return 0; }
+            if (number == 1) { return 1; }
+
+            return Fibonacci(number - 1) + Fibonacci(number - 2);
+        }
     }
 }
